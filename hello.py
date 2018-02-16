47e2030e4782eb73ae0a7962f44b40a9c3ab7901
JLeop <julian.leopold@student.unisg.ch>
2018-02-16 10:40:07 +0100

    hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+# file is green since it is not added to the git yet
+def hello_world(name = input("what is your name?   ")):
+    print("hello world, hello", name)
+
+if __name__ == "__main__":
+    hello_world()
