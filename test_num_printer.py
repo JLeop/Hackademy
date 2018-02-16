63e10d24b1ddbc10b0559f58f2e4975f653454ad
Julian <julian.leopold@student.unisg.ch>
2018-02-16 14:28:13 +0100

    test_num_printer.py

---
+++ test_num_printer.py
@@ -0,0 +1,11 @@
+import unittest
+import production
+
+class Test_nasty_code(unittest.TestCase):
+    def test_print_square(self):
+        input = list(range(0,10))
+        output = [0,1,4,9,16,25,36,49,64,81,100]
+        self.assertListEqual(output,nasty_code.print_square(input))
+
+if __name__ == "__main__":
+    unittest.main()
